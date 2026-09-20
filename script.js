1012c72195a4cd2d000057ff4171b6046379f4e3
Al-ameen <alameenabiola3@gmail.com>
2026-09-20 08:29:30 -0400

    add playgame function and number of rounds

--- script.js
+++ script.js
@@ -2,53 +2,89 @@ console.log("Hello World!")
 
 // Get Human's choice
 function getHumanChoice(hchoice) {
-    hchoice = prompt("Enter your choice between - Rock, Paper, Scissors.")
+    hchoice = prompt("Enter your choice between - Rock, Paper, Scissors.").toUpperCase()
     return hchoice
 }
 
 // Get computer's choice
 function getComputerChoice(choices) {
     
-    choices = ['Rock', 'Paper', 'Scissors']
+    choices = ['ROCK', 'PAPER', 'SCISSORS']
     const randomIndex = Math.floor(Math.random() * choices.length)
     return choices[randomIndex]
 }
 
-const humanScore = 0
-const comptuerScore = 0
+let humanScore = 0
+let computerScore = 0
 
 function playRound(humanChoice, computerChoice) {
     
 
     if (humanChoice === computerChoice) {
-        console.log("A tie!!")
+        console.log(`A tie!! you both chose ${humanChoice}`)
     }else if (humanChoice === 'ROCK' && computerChoice === 'PAPER' || humanChoice === 'PAPER' && computerChoice === 'ROCK'){
         if (humanChoice === 'PAPER') {
-            console.log(`You won!! you beat ${computerChoice}`)
+            console.log(`You won!! ${humanChoice} covers ${computerChoice}`)
+            humanScore++
         }else {
-            console.log(`Computer Won!! it chooses ${computerChoice}`)
+            console.log(`You lose!! ${computerChoice} covers ${humanChoice}`)
+            computerScore++
         }
         // console.log("PAPER won!! it beats ROCK")
 
     }else if (humanChoice === 'ROCK' && computerChoice === 'SCISSORS' || humanChoice === 'SCISSORS' && computerChoice === 'ROCK'){
         if (humanChoice === 'ROCK') {
-            console.log(`You won!! you beat ${computerChoice}`)
+            console.log(`You won!! ${humanChoice} breaks ${computerChoice}`)
+            humanScore++
         }else {
-            console.log(`Computer Won!! it chooses ${computerChoice}`)
+            console.log(`You lose!! ${computerChoice} breaks ${humanChoice}`)
+            computerScore++
         }
         // console.log("ROCK won!! it beats SCISSORS")
     }else if (humanChoice === 'SCISSORS' && computerChoice === 'PAPER' || humanChoice === 'PAPER' && computerChoice === 'SCISSORS'){
         if (humanChoice === 'SCISSORS') {
-            console.log(`You won!! you beat ${computerChoice}`)
+            console.log(`You won!! ${humanChoice} cuts ${computerChoice}`)
+            humanScore++
         }else {
-            console.log(`Computer Won!! it chooses ${computerChoice}`)
+            console.log(`You lose!! ${computerChoice} cuts ${humanChoice}`)
+            computerScore++
         }
         // console.log("SCISSORS won! it beats PAPER")
     }
+
+    
 }
 
-const humanSelection = getHumanChoice().toUpperCase();
-const computerSelection = getComputerChoice().toUpperCase();
 
 
-playRound(humanSelection, computerSelection);
+
+function playGame() {
+
+    console.log("--- Round 1 ---");
+    playRound(getHumanChoice(), getComputerChoice())
+    console.log("--- Round 2 ---");
+    playRound(getHumanChoice(), getComputerChoice())
+    console.log("--- Round 3 ---");
+    playRound(getHumanChoice(), getComputerChoice())
+    console.log("--- Round 4 ---");
+    playRound(getHumanChoice(), getComputerChoice())
+    console.log("--- Round 5 ---");
+    playRound(getHumanChoice(), getComputerChoice())
+
+    console.log("--- Game Over ---")
+    console.log(`Your score: ${humanScore}`)
+    console.log(`Computer score: ${computerScore}`)
+
+    if (humanScore > computerScore) {
+        console.log("You Won!!")
+
+    }else if (humanScore < computerScore) {
+        console.log("You lost!!")
+    }else {
+        console.log("A tie!!")
+    }
+}
+
+
+playGame()
+
